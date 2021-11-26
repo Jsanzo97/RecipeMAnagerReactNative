5c280e9211fc13ba74ddacbeeed55982a65c2c85
Jsanzo97 <jorgesanzo21@gmail.com>
2021-11-26 21:12:49 +0100

    Fix modules for navigation

---
+++ Login/Login.js
@@ -0,0 +1,223 @@
+import 'react-native-gesture-handler';
+import React from 'react';
+import type {Node} from 'react';
+import {
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+  Image,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import {Colors, Dimens} from '../Common/Constants'
+import {Strings} from '../Common/Strings'
+import RecipeManagerApi from '../Network/RecipeManagerApi'
+import CustomDialog from '../Common/CustomDialog'
+import {NavigationContainer} from '@react-navigation/native'
+
+export default class Login extends React.Component {
+
+  constructor(props) {
+   super(props)
+    this.state = {
+      email: '',
+      password: '',
+      showDialog: false,
+      titleDialog: '',
+      messageDialog: '',
+      acceptHandleAction: () => {},
+      cancelHandleAction: () => {}
+    }
+    this.recipeManagerApi = new RecipeManagerApi()
+  }
+
+  onLoginAction = () => {
+    if(this.checkValidFields()) {
+      this.recipeManagerApi.login(this.state.email, this.state.password)
+        .then(response => {
+            if (response.success) {
+              this.onSuccessLoginOperation(response.successMessage)
+            } else {
+              this.onErrorOperation(response.errorMessage)
+            }
+          }
+        )
+    } else {
+      this.onErrorOperation(Strings.loginScreenErrorEmptyFields)
+    }
+  }
+
+  onSignUpAction = () => {
+    if(this.checkValidFields()) {
+      this.recipeManagerApi.signUp(this.state.email, this.state.password)
+        .then(response => {
+          if (response.success) {
+            this.onSuccessSignUpOperation(response.successMessage)
+          } else {
+            this.onErrorOperation(response.errorMessage)
+          }
+        }
+      )
+    } else {
+      this.onErrorOperation(Strings.loginScreenErrorEmptyFields)
+    }
+  }
+
+  checkValidFields = () => {
+    return this.state.email != '' && this.state.password != ''
+  }
+
+  onSuccessLoginOperation = (message) => {
+    this.showMessage(
+      Strings.sucessOperation,
+      message,
+      () => {
+        this.setState({
+          showDialog: false
+        })
+        // Navigate to home screen
+        },
+      () => {
+        this.setState({
+          showDialog: false
+        })
+      }
+    )
+  }
+
+  onSuccessSignUpOperation = (message) => {
+    this.showMessage(
+      Strings.sucessOperation,
+      message,
+      () => {
+        this.setState({
+          showDialog: false
+        })
+      },
+      () => {
+        this.setState({
+          showDialog: false
+        })
+      }
+    )
+  }
+
+  onErrorOperation = (message) => {
+    this.showMessage(
+      Strings.errorOperation,
+      message,
+      () => {
+        this.setState({
+          showDialog: false
+        })
+      },
+      () => {
+        this.setState({
+          showDialog: false
+        })
+      }
+    )
+  }
+
+  showMessage = (title, message, acceptHandleAction, cancelHandleAction) => {
+    this.setState({
+      showDialog: true,
+      titleDialog: title,
+      messageDialog: message,
+      acceptHandleAction: acceptHandleAction,
+      cancelHandleAction: cancelHandleAction
+    })
+  }
+
+  render() {
+
+    return (
+      <SafeAreaView style = {appStyle.container}>
+        <StatusBar backgroundColor = {Colors.primaryColor} barStyle = 'light-content'/>
+        
+        <Image style={appStyle.logo} source={require('../Resources/splash_login_bg.png')}/>
+        <Text style = {appStyle.title}> Recipe Manager</Text>
+       
+        <View style = {appStyle.inputView} >
+          <TextInput  
+            style = {appStyle.inputText}
+            placeholder = 'Email' 
+            placeholderTextColor = {Colors.primaryColor}
+            onChangeText = {text => this.setState({email: text})}/>
+        </View>
+        
+        <View style = {appStyle.inputView} >
+          <TextInput  
+            secureTextEntry
+            style = {appStyle.inputText}
+            placeholder = 'Password' 
+            placeholderTextColor = {Colors.primaryColor}
+            onChangeText = {text => this.setState({password: text})}/>
+        </View>
+
+        <View style = {{flexDirection: 'row'}}>
+          <TouchableOpacity style = {appStyle.loginButton} onPress = {this.onSignUpAction}>
+            <Text style = {appStyle.loginText}>Sign up</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={appStyle.loginButton} onPress = {this.onLoginAction}>
+            <Text style = {appStyle.loginText}>Log in</Text>
+          </TouchableOpacity>
+        </View>
+
+        <CustomDialog visible = {this.state.showDialog} title = {this.state.titleDialog} message = {this.state.messageDialog}
+          acceptHandleAction = {this.state.acceptHandleAction} cancelHandleAction = {this.state.cancelHandleAction}/>
+  
+      </SafeAreaView>
+    );
+  }
+}
+
+const appStyle = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    backgroundColor: Colors.white
+  },
+  logo: {
+    marginTop: '20%',
+    width: '50%',
+    height: '30%'
+  },
+  title: {
+    fontWeight: 'bold',
+    fontSize: Dimens.big,
+    color: Colors.primaryColor,
+    marginBottom: Dimens.small
+  },
+  inputView: {
+    width: '75%',
+    backgroundColor: Colors.white,
+    height: Dimens.huge,
+    marginBottom: Dimens.small,
+    justifyContent: 'center',
+    padding: Dimens.small
+  },
+  inputText: {
+    height: Dimens.huge,
+    color: Colors.primaryColor,
+    borderBottomColor: Colors.primaryColor,
+    borderBottomWidth: 2
+  },
+  loginButton: {
+    width: '35%',
+    backgroundColor: Colors.primaryColor,
+    borderRadius: Dimens.small,
+    height: Dimens.big,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: Dimens.medium,
+    marginLeft: Dimens.small,
+    marginRight: Dimens.small
+  },
+  loginText: {
+    color: Colors.white,
+    fontSize: Dimens.medium
+  }
+});
